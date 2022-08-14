d7808544c1c21363b550a65adf1a6adae01f4633
felipe <felipe.calderon321@gmail.com>
2022-08-14 18:02:24 -0400

    importacion rama de diego

---
+++ rutas/routes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const route = express.Router();
+const passport = require("passport");
+
+route.use(express.json());
+route.use(express.urlencoded({ extended: true }));
+
+const controlers = require("../src/controler");
+
+// ---------------------------------------------------------
+// root/pagina principal
+route.get("/", controlers.root);
+
+// login
+route.get("/login", controlers.getLogin);
+route.post(
+  "/login",
+  passport.authenticate("login", { failureRedirect: "/nologin" }),
+  controlers.postLogin
+);
+route.get("/nologin", controlers.noLogin)
+
+// registro
+route.get("/signout", controlers.getSignout);
+route.post(
+  "/signout",
+  passport.authenticate("signup", { failureRedirect: "/nosignout" }),
+  controlers.postSignout
+);
+route.get("/nosignout", controlers.noSignout)
+
+// hago este endpoint para probar si funciona el logeo
+// luego de hacer un /login de manera correcta se deberia mostrar "EL USUARIO ESTA LOGEADO ACTUALMENTE"
+// en caso de no estarlo se lo deberia redireccionar a la pagina principal.
+route.get("/userLogged", controlers.userLogged);
+
+
+module.exports = route;
--- server.js
+++ server.js
@@ -1,46 +1,8 @@
-//llamado a módulos y def de puertos
-const express = require("express");
+const app = require('./rutas/login');
+const PORT = process.env.PORT || 3000
 
-//hola desde rama Felipe
-
-const app = express();
-const port = 3050
-
-// parse solicitudes get y post
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-
-//conexión a mongoDB - SEGURIDAD CON VARIABLES DE ENTORNO
-var mongoose = require('mongoose');
-
-const usr = 'usertest'
-const pwr = 'tnZ1oURXdYDPmr9X'
-const db = 'sysgym'
-const uri = `mongodb+srv://${usr}:${pwr}@cluster0.1xynyxj.mongodb.net/${db}?retryWrites=true&w=majority`;
-mongoose.connect(uri, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true, 
-})
-.then(() => console.log('conectado a mongo'))
-.catch(e => console.log(e))
-
-//ruta demo passport
-var ingreso = require('./rutas/ingreso');
-app.use('/ingreso', ingreso);
-
-app.use('/vistaform', authRouter);
-//Ruta ingreso usuario (login):
-const login = require('./rutas/login')
-
-//Ruta sistema (despues de inicio):
-const sistema = require('./rutas/sistema')
-
-//Llamando a las rutas:
-app.use(login)
-app.use(sistema)
 
 //Puerto
-app.listen(port, () => {
-  console.log(`Funcionando en http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Funcionando en http://localhost:${PORT}`);
 });
