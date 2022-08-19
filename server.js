a8220c370ca11e05f45245d74171d1bc87367011
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-08-19 09:28:38 -0300

    agrega cambios

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Factory = require('./utils/factory')
 
 const DBSChosen = process.argv[2] || "mongo";
 const DBS = Factory.getInstance(DBSChosen);
+DBS.connection(DBSChosen);
 //Puerto
 app.listen(PORT, () => {
   console.log(`Funcionando en http://localhost:${PORT}`);
--- utils/factory.js
+++ utils/factory.js
@@ -1,4 +1,7 @@
-const config = require('dotenv').config
+const mongoose = require('mongoose')
+require("dotenv").config();
+
+const MONGO_URI = process.env.MONGO_URI;
 let instance = null;
 console.log(MONGO_URI);
 
@@ -18,10 +21,11 @@ class Factory {
   connection(data) {
     if (data == "file") {
     } else if (data == "mongo") {
-      mongoose.connect(config.MONGO_URI, {
+      mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
+      console.log("CONEXION MONGO");
     } else {
       console.log("Error en conexion de base de datos");
     }
