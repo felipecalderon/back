e796299b584abea6c4dc78692cdd5783089f2d54
felipe <felipe.calderon321@gmail.com>
2022-08-20 14:10:24 -0400

    comentando el codigo y ajustando token

--- src/registro.js
+++ src/registro.js
@@ -3,19 +3,20 @@ const createHash = require("../utils/hasGenerator");
 
 //Constrolador del registro de alumnos en mongo
 exports.postRegistro = async (req, res) => {
+  // verifica si el correo existe
   const emailExiste = await Usuario.findOne({ email: req.body.email });
   if (emailExiste) {
     return res.status(400).json({
       error: "correo ya registrado",
     });
   }
-
+  // crea usuario
   const usuario = new Usuario({
     name: req.body.name,
     email: req.body.email,
     password: createHash(req.body.password),
   });
-
+  // almacena algún error si lo hubiese
   try {
     const UsuarioBD = await usuario.save();
     res.json({
