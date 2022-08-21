56bc6d905e37655fc134f7d72da5cb7b7ebfb812
felipe <felipe.calderon321@gmail.com>
2022-08-20 20:19:25 -0400

    restauracion exitosa

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 console.clear();
-require("dotenv").config();
-const app = require("./rutas/routes");
-const mongoose = require("mongoose");
+const app = require("./rutas/login");
 const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-//URIMONGO = `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWR}@${process.env.MONGO_CLOUD}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+URIMONGO = `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWR}@${process.env.MONGO_CLOUD}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+//llamando a mongoose
+mongoose
+  .connect(URIMONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("conectado a mongo"))
+  .catch((e) => console.log(e));
 
-<<<<<<< HEAD
-//Puert
-=======
-const DBSChosen = process.argv[2] || "mongo";
-const DBS = Factory.getInstance(DBSChosen);
-DBS.connection(DBSChosen);
 //Puerto
->>>>>>> 7cc19ea2787f50d313b4087cef0fd98790f05947
 app.listen(PORT, () => {
   console.log(`Funcionando en http://localhost:${PORT}`);
 });
--- src/registro.js
+++ src/registro.js
@@ -1,21 +1,22 @@
-const Usuario = require("../../modelos/usuario");
-const createHash = require("../../utils/hasGenerator");
+const Usuario = require("../modelos/usuario");
+const createHash = require("../utils/hasGenerator");
 
 //Constrolador del registro de alumnos en mongo
 exports.postRegistro = async (req, res) => {
+  // verifica si el correo existe
   const emailExiste = await Usuario.findOne({ email: req.body.email });
   if (emailExiste) {
     return res.status(400).json({
       error: "correo ya registrado",
     });
   }
-
+  // crea usuario
   const usuario = new Usuario({
     name: req.body.name,
     email: req.body.email,
     password: createHash(req.body.password),
   });
-
+  // almacena algún error si lo hubiese
   try {
     const UsuarioBD = await usuario.save();
     res.json({
