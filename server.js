8fdb23965f174e530fb46497c68d9b346e95602f
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-08-23 10:57:36 -0300

    cambios

--- server.js
+++ server.js
@@ -2,6 +2,16 @@
 const app = require("./rutas/login");
 const PORT = process.env.PORT || 3000;
 const Factory = require("./utils/factory");
+const mongoose = require('mongoose')
+URIMONGO = `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWR}@${process.env.MONGO_CLOUD}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+//llamando a mongoose
+mongoose
+  .connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("conectado a mongo"))
+  .catch((e) => console.log(e));
 
 
 //Puerto
