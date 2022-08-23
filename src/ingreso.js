55977c3ef0d9405f7c7024b9feae36e932b1d1d4
felipe <felipe.calderon321@gmail.com>
2022-08-23 08:16:49 -0400

    respondiendo 401 en caso error login

--- src/ingreso.js
+++ src/ingreso.js
@@ -9,7 +9,7 @@ exports.postIngreso = async (req, res) => {
     $or: [{ email: req.body.email }, { username: req.body.email }],
   });
   if (!usernameExiste) {
-    return res.status(400).json({
+    return res.status(401).json({
       error: "Usuario o correo incorrecto",
     });
   }
@@ -22,7 +22,7 @@ exports.postIngreso = async (req, res) => {
     function (error, bien) {
       // si ocurre un error con la comparación del form con la data de mongo se mostrara acá
       if (error) {
-        res.status(400).json(error);
+        res.status(401).json(error);
       }
 
       // si la clave del form es la misma que la hasheada en mongo se genera el token
@@ -45,7 +45,7 @@ exports.postIngreso = async (req, res) => {
 
         // callback con la respuesta negativa en caso que las claves no coincidan
       } else {
-        return res.status(400).json({ error: "Clave no coincide" });
+        return res.status(401).json({ error: "Clave no coincide" });
       }
     }
   );
