5a3d381742e9f2c86045bb743e61c1a97ad08ed5
felipe <felipe.calderon321@gmail.com>
2022-08-23 07:52:02 -0400

    agregando CORS

--- rutas/routes.js
+++ rutas/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const route = express.Router();
 
 //Validadores (middlewares)
 const validatoken = require("../middlewares/validatoken");
 
+route.use(cors());
 route.use(express.json());
 route.use(express.urlencoded({ extended: true }));
 
