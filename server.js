68db399c980449f9bf4be7748574b87c326d6e8d
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-08-24 12:13:28 -0300

    agrega .use en SERVER

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const route = require('./routes/home')
 const PORT = process.env.PORT || 3000;
 const Factory = require("./utils/factory");
 
@@ -8,7 +8,7 @@ const DBSChosen = process.argv[2] || "mongo";
 const DBS = Factory.getInstance(DBSChosen);
 DBS.connection(DBSChosen);
 
-
+app.use(route)
 const SERVER = app.listen(PORT, () => {
   console.log(`Funcionando en http://localhost:${PORT}`);
 });
